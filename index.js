412ccb819db90e94ad775f448aa0caa6c5930c52
daniellykammers <dany.kammers@hotmail.com>
2023-02-05 16:25:48 -0300

    feat(M001S002): iniciação do projeto

---
+++ index.js
@@ -0,0 +1,19 @@
+var mesAtual = 'janeiro';
+
+// Mês
+document.getElementById('month').innerHTML = mesAtual;
+
+// Entradas
+document.getElementById('total-earnings').innerHTML = 2000;
+
+// Saídas
+document.getElementById('total-expenses').innerHTML = 1800;
+
+// Saldo
+document.getElementById('final-balance').innerHTML = 200;
+
+// Minha receita: Salário
+document.getElementById('salary').innerHTML = 2000;
+
+// Meus gastos: Alimentação
+document.getElementById('food').innerHTML = 200;
