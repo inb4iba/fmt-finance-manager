4692daf30d48b22fed3d307141bdddba932f181b
daniellykammers <dany.kammers@hotmail.com>
2023-02-11 16:57:14 -0300

    feat(M01S002): estruturas de repeticao

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ var mesAtual = 'janeiro';
 var entradas = 8000;
 var saidas = 400;
 var saldo = 1000;
-var salario = {
-  description: 'Salário',
-  value: 4000,
-};
-var alimentacao = 800;
+
+var earningsList = [
+  {
+    description: 'Salário',
+    value: 4000,
+  },
+  {
+    description: 'Freela',
+    value: 400,
+  },
+];
+
+var expensesList = [
+  {
+    description: 'Alimentação',
+    value: 650,
+  },
+  {
+    description: 'PSN',
+    value: 40,
+  },
+  {
+    description: 'Energia',
+    value: 500,
+  },
+];
 
 // Mês
 document.getElementById('month').innerHTML = mesAtual;
@@ -20,10 +41,16 @@ document.getElementById('total-expenses').innerHTML = saidas;
 // Saldo
 document.getElementById('final-balance').innerHTML = saldo;
 
-// Minha receita: Salário
-document.getElementById(
-  'salary'
-).innerHTML = `${salario.description}: R$ ${salario.value}`;
+for (var i = 0; i < earningsList.length; i++) {
+  var li = document.createElement('li');
+  li.innerText = `${earningsList[i].description}: R$ ${expensesList[i].value}`;
 
-// Meus gastos: Alimentação
-document.getElementById('food').innerHTML = alimentacao;
+  document.getElementById('earnings-list').appendChild(li);
+}
+
+for (var i = 0; i < expensesList.length; i++) {
+  var li = document.createElement('li');
+  li.innerText = `${expensesList[i].description}: R$ ${expensesList[i].value}`;
+
+  document.getElementById('expenses-list').appendChild(li);
+}
